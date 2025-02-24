e772fdd879d04dcaad8d73cc85777be0cf5a6871
AusBruce <113293507+AusBruce@users.noreply.github.com>
2025-02-24 16:35:10 +1100

    config jsx run time and start component

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,20 +1,32 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true,
+    es2020: true,
+  },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
+    'plugin:react/jsx-runtime', // Enables automatic JSX runtime (React 17+)
+    'plugin:react-hooks/recommended', // Recommended React Hooks rules
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  settings: {
+    react: {
+      version: 'detect', // Automatically detect the installed React version
+    },
+  },
   plugins: ['react-refresh'],
   rules: {
+    'no-unused-vars': 'warn', // Warn for unused variables (instead of throwing an error)
+    'react/react-in-jsx-scope': 'off', // React 17+ doesn't require importing React explicitly
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
   },
-}
+};
